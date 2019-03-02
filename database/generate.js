0e8b9d4e9542545912d0981527a6317dafcc4c78
Bill Mortimer <bill@billisit.com>
2019-03-02 14:23:36 -0800

    Scaling the generation of data working.

--- database/generate.js
+++ database/generate.js
@@ -34,71 +34,96 @@ const Looks = db.define('Looks', {
 
 */
 
-var pant_mods = ['SKI', 'GOLF', 'INSULATED', 'LIGHTWEIGHT', 'FLEECE', 'PRO', 'HEAVYWEIGHT', 'WARM-UP', 'TRACK', 'TRAINING', 'COMPRESSION', 'CLASSIC', 'PERFORMANCE', 'ATHLETIC', 'FITTED', 'THERMAL', 'KNIT', 'TERRY', 'RAIN', 'BASKETBALL', 'FOOTBALL', 'BASEBALL', 'CORE', 'TECH', 'STRETCH'];
+// Constants for Pants Looks
+const pant_mods = ['SKI', 'GOLF', 'INSULATED', 'LIGHTWEIGHT', 'FLEECE', 'PRO', 'HEAVYWEIGHT', 'WARM-UP', 'TRACK', 'TRAINING', 'COMPRESSION', 'CLASSIC', 'PERFORMANCE', 'ATHLETIC', 'FITTED', 'THERMAL', 'KNIT', 'TERRY', 'RAIN', 'BASKETBALL', 'FOOTBALL', 'BASEBALL', 'CORE', 'TECH', 'STRETCH'];
+const pant_types = ['PANTS', 'SHORTS', 'JOGGERS', 'TIGHTS', 'SWEATPANTS'];
+const pant_base_price = 20;
+const pant_rand_price = 115;
 
-var pant_types = ['PANTS', 'SHORTS', 'JOGGERS', 'TIGHTS', 'SWEATPANTS'];
+// Constants for Shirts Looks
+const shirt_mods = ['CLASSIC', 'TREFOIL', 'SPORT', 'ESSENTIAL', 'PERFORMANCE', 'LONG SLEEVE', 'COMPRESSION', 'FITTED', 'TRAINING', 'SLEEVELESS', 'TECH', 'GRAPHIC', 'COMPRESSION'];
+const shirt_types = ['TEE', 'POLO', 'SHIRT', 'JERSEY', 'TANK TOP', 'TOP', 'CREW', 'HENLEY', 'MOCK NECK'];
+const shirt_base_price = 20;
+const shirt_rand_price = 115;
 
-var shirt_mods = ['CLASSIC', 'TREFOIL', 'SPORT', 'ESSENTIAL', 'PERFORMANCE', 'LONG SLEEVE', 'COMPRESSION', 'FITTED', 'TRAINING', 'SLEEVELESS', 'TECH', 'GRAPHIC', 'COMPRESSION'];
+// Constants for Jacket Looks
+const jacket_mods = ['TRACK', 'DOWN', 'TRAINING', 'SOFT', 'HALF-ZIP', 'FULL-ZIP', 'INSULATED', 'LIGHTWEIGHT', 'FLEECE', 'PRO', 'HEAVYWEIGHT', 'WARM-UP', 'CLASSIC', 'PERFORMANCE', 'ATHLETIC', 'FITTED', 'THERMAL', 'KNIT', 'TERRY', 'RAIN', 'BASKETBALL', 'FOOTBALL', 'BASEBALL', 'CORE', 'TECH'];
+const jacket_types = ['JACKET', 'WINDBREAKER', 'PULLOVER', 'HOODIE', 'SWEATSHIRT', 'COAT', 'SHELL', 'VEST', 'SWEATER'];
+const jacket_base_price = 20;
+const jacket_rand_price = 115;
 
-var shirt_types = ['TEE', 'POLO', 'SHIRT', 'JERSEY', 'TANK TOP', 'TOP', 'CREW', 'HENLEY', 'MOCK NECK'];
+// The array that will hold the objects to be loaded
+const looksArr = [];
 
-var jacket_mods = ['TRACK', 'DOWN', 'TRAINING', 'SOFT', 'HALF-ZIP', 'FULL-ZIP', 'INSULATED', 'LIGHTWEIGHT', 'FLEECE', 'PRO', 'HEAVYWEIGHT', 'WARM-UP', 'CLASSIC', 'PERFORMANCE', 'ATHLETIC', 'FITTED', 'THERMAL', 'KNIT', 'TERRY', 'RAIN', 'BASKETBALL', 'FOOTBALL', 'BASEBALL', 'CORE', 'TECH'];
-
-var jacket_types = ['JACKET', 'WINDBREAKER', 'PULLOVER', 'HOODIE', 'SWEATSHIRT', 'COAT', 'SHELL', 'VEST', 'SWEATER'];
-
-
-var createNewLook = (lookObj, i) => {
+// Populate an existing single look object, or create a new one
+var createNewLook = (looksArrIndex, index) => {
+  //console.log(`createNewLook: looksArrIndex = ${looksArrIndex}, index = ${index}`);
+  let lookObj = looksArr[looksArrIndex] || {};
+  //console.log(lookObj);
+  // object ids are index + 1
+  let i = index + 1;
   // Helper function: Generate a name that has an optional first part of the name 
   // OR an optional second part of the name OR both, and a required third part of the name.
   let generateName = (modArr, typeArr) => {
+    // 50% chance of a random first string
     let retStr = (Math.random() >= 0.5) ? faker.random.word().split(' ')[0].toUpperCase() + ' ' : '';
+    // 50% chance of generating a second string if the first string exists, otherwise 100%
     retStr += (retStr.length) ? 
                (Math.random() >= 0.5) ? modArr[Math.floor(Math.random() * modArr.length)] + ' ' : ''
              : modArr[Math.floor(Math.random() * modArr.length)] + ' ';
+    // Always generate the third string
     retStr += typeArr[Math.floor(Math.random() * typeArr.length)];
     return retStr;
   }
-  // create fake data for it
+  // assign values to this object
   lookObj.id = i;
   lookObj.pant_name = generateName(pant_mods, pant_types);
   lookObj.pant_url = 'https://loremflickr.com/295/295/pants?random' + i;
-  lookObj.pant_price = Math.floor(Math.random() * 115) + 20; 
+  lookObj.pant_price = Math.floor(Math.random() * pant_rand_price) + pant_base_price; 
   lookObj.shirt_name = generateName(shirt_mods, shirt_types);
   lookObj.shirt_url = 'https://loremflickr.com/295/295/shirt?random' + i;
-  lookObj.shirt_price = Math.floor(Math.random() * 100) + 15;
+  lookObj.shirt_price = Math.floor(Math.random() * shirt_rand_price) + shirt_base_price;
   lookObj.jacket_name = generateName(jacket_mods, jacket_types);
   lookObj.jacket_url = 'https://loremflickr.com/295/295/jacket?random' + i;
-  lookObj.jacket_price = Math.floor(Math.random() * 200) + 30;
+  lookObj.jacket_price = Math.floor(Math.random() * jacket_rand_price) + jacket_base_price;
 
   return lookObj;
 }
 
-// lookObj.pant_name = faker.random.word().split(' ')[0].toUpperCase() + ' ' : ''
-// + pant_mods[Math.floor(Math.random() * pant_mods.length)] + ' ' 
-// + pant_types[Math.floor(Math.random() * pant_types.length)];
-
-// lookObj.shirt_name = faker.random.word().split(' ')[0].toUpperCase() + ' '  
-// + shirt_mods[Math.floor(Math.random() * shirt_mods.length)] + ' ' 
-// + shirt_types[Math.floor(Math.random() * shirt_types.length)];
-
-// lookObj.jacket_name = faker.random.word().split(' ')[0].toUpperCase() + ' ' 
-// + jacket_mods[Math.floor(Math.random() * jacket_mods.length)] + ' ' 
-// + jacket_types[Math.floor(Math.random() * jacket_types.length)];
-
-var createNLookObjs = (n) => {
-  let looksArr = [];
-  for (let i = 1; i <= n; i++) {
+var createNLookObjs = (n, startIndex) => {
+  //console.log(`createNLookObjs: n = ${n}, startIndex = ${startIndex}`);
+  for (let i = 0; i < n; i++) {
     // create a new object
-    let lookObj = createNewLook({}, i);
-    // push it into the array
-    looksArr.push(lookObj);
+    let newLook = createNewLook(i, startIndex + i);
+    // push it into the array if the array doesn't already have a value there
+    if (!(looksArr[i])) {
+      looksArr.push(newLook);
+    }
   }
-  return looksArr;
 }
 
-console.time('createAndWriteData');
+var loadLookObjs = (numBatches, perBatch) => {
+  // For each batch
+  for (let i = 0; i < numBatches; i++) {
+    // create perBatch look objects and load them into the looksArr
+    let startingIndex = i * perBatch;
+    createNLookObjs(perBatch, startingIndex);
+    // load the looksArr into the database
+    //console.log(JSON.stringify(looksArr));
+  }
+}
 
-var finalLooksArr = createNLookObjs(100000);
+console.log('starting -----')
+console.time('createAndWriteData');
+// Refactoring to re-use the same array and objects
+// looksObj is now a global array declared above
+// Total number of objects to generate
+let totalToGenerate = 10000000;
+// Total number of objects before recycling; assume divides equally into TotalToGenerate
+let totalPerBatch = 10000;
+// load 
+loadLookObjs(totalToGenerate/totalPerBatch, totalPerBatch);
+console.log(`totalToGenerate = ${totalToGenerate}, totalPerBatch = ${totalPerBatch}, Batches = ${totalToGenerate/totalPerBatch}`);
 
 //str = JSON.stringify(finalLooksArr);
 
