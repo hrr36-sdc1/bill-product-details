28620ee1b54f8bee46cf4d35c65a91a922a13acc
billmortimer <bill@billisit.com>
2019-03-11 12:08:44 -0700

    Contains all code for generating data into CSV file, and a short example file.

--- database/generate.js
+++ database/generate.js
@@ -1,4 +1,4 @@
-const {db, Looks} = require('./index.js');
+const db = require('./index.js');  // Removed Looks since using copy to load data into postgres
 var faker = require('faker');
 var fs = require('fs');
 
@@ -45,27 +45,29 @@ const pant_rand_price = 115;
 // Constants for Shirts Looks
 const shirt_mods = ['CLASSIC', 'TREFOIL', 'SPORT', 'ESSENTIAL', 'PERFORMANCE', 'LONG SLEEVE', 'COMPRESSION', 'FITTED', 'TRAINING', 'SLEEVELESS', 'TECH', 'GRAPHIC', 'COMPRESSION'];
 const shirt_types = ['TEE', 'POLO', 'SHIRT', 'JERSEY', 'TANK TOP', 'TOP', 'CREW', 'HENLEY', 'MOCK NECK'];
-const shirt_base_price = 20;
-const shirt_rand_price = 115;
+const shirt_base_price = 15;
+const shirt_rand_price = 125;
 
 // Constants for Jacket Looks
 const jacket_mods = ['TRACK', 'DOWN', 'TRAINING', 'SOFT', 'HALF-ZIP', 'FULL-ZIP', 'INSULATED', 'LIGHTWEIGHT', 'FLEECE', 'PRO', 'HEAVYWEIGHT', 'WARM-UP', 'CLASSIC', 'PERFORMANCE', 'ATHLETIC', 'FITTED', 'THERMAL', 'KNIT', 'TERRY', 'RAIN', 'BASKETBALL', 'FOOTBALL', 'BASEBALL', 'CORE', 'TECH'];
 const jacket_types = ['JACKET', 'WINDBREAKER', 'PULLOVER', 'HOODIE', 'SWEATSHIRT', 'COAT', 'SHELL', 'VEST', 'SWEATER'];
-const jacket_base_price = 20;
-const jacket_rand_price = 115;
+const jacket_base_price = 40;
+const jacket_rand_price = 125;
 
-// The array that will hold the objects to be loaded
-const looksArr = [];
+// Rewrite data generation and loading to write all data out as a single CSV file
+// Change data loading into postgres to use copy to load data from the CSV file
 
-// Populate an existing single look object, or create a new one
-var createNewLook = (looksArrIndex, index) => {
-  //console.log(`createNewLook: looksArrIndex = ${looksArrIndex}, index = ${index}`);
-  let lookObj = looksArr[looksArrIndex] || {};
-  //console.log(lookObj);
+// Create a stream to a file using fs
+const myCSVStream = fs.createWriteStream('./database/looksdata.csv');
+
+// Re-written to create a single csv string
+var createNewCSVStr = (index) => {
   // object ids are index + 1
   let i = index + 1;
+
   // Helper function: Generate a name that has an optional first part of the name 
-  // OR an optional second part of the name OR both, and a required third part of the name.
+  // OR an optional second part of the name OR both, 
+  // AND a required third part of the name.
   let generateName = (modArr, typeArr) => {
     // 50% chance of a random first string
     let retStr = (Math.random() >= 0.5) ? faker.random.word().split(' ')[0].toUpperCase() + ' ' : '';
@@ -77,78 +79,84 @@ var createNewLook = (looksArrIndex, index) => {
     retStr += typeArr[Math.floor(Math.random() * typeArr.length)];
     return retStr;
   }
-  // assign values to this object
-  lookObj.id = i;
-  lookObj.pant_name = generateName(pant_mods, pant_types);
-  lookObj.pant_url = 'https://loremflickr.com/295/295/pants?random' + i;
-  lookObj.pant_price = Math.floor(Math.random() * pant_rand_price) + pant_base_price; 
-  lookObj.shirt_name = generateName(shirt_mods, shirt_types);
-  lookObj.shirt_url = 'https://loremflickr.com/295/295/shirt?random' + i;
-  lookObj.shirt_price = Math.floor(Math.random() * shirt_rand_price) + shirt_base_price;
-  lookObj.jacket_name = generateName(jacket_mods, jacket_types);
-  lookObj.jacket_url = 'https://loremflickr.com/295/295/jacket?random' + i;
-  lookObj.jacket_price = Math.floor(Math.random() * jacket_rand_price) + jacket_base_price;
 
-  return lookObj;
+  // create the CSV string
+  
+  let csvStr = i +',';
+  csvStr += generateName(pant_mods, pant_types) + ',';
+  csvStr += 'https://loremflickr.com/295/295/pants?random' + i +',';
+  csvStr += Math.floor(Math.random() * pant_rand_price) + pant_base_price + ','; 
+  csvStr += generateName(shirt_mods, shirt_types) + ',';
+  csvStr += 'https://loremflickr.com/295/295/shirt?random' + i + ',';
+  csvStr += Math.floor(Math.random() * shirt_rand_price) + shirt_base_price + ',';
+  csvStr += generateName(jacket_mods, jacket_types) +',';
+  csvStr += 'https://loremflickr.com/295/295/jacket?random' + i +',';
+  csvStr += Math.floor(Math.random() * jacket_rand_price) + jacket_base_price + '\n';
+
+  return csvStr;
 }
 
-var createNLookObjs = (n, startIndex) => {
+// Creates and writes N CSV Strings to a file
+// Remove parameters and move i and n to global variables so you can restart if the write fails.
+var idx = 0; // start at 0
+var N = 10000000;  // How many records to create and write
+var createAndWriteNCSVStrings = () => {
   //console.log(`createNLookObjs: n = ${n}, startIndex = ${startIndex}`);
-  for (let i = 0; i < n; i++) {
-    // create a new object
-    let newLook = createNewLook(i, startIndex + i);
-    // push it into the array if the array doesn't already have a value there
-    if (!(looksArr[i])) {
-      looksArr.push(newLook);
+  // for (let i = 0; i < n; i++) {
+  while(idx < N) {
+    // create a new string
+    let newCSVStr = createNewCSVStr(idx);
+    //console.log(newCSVStr);
+    // increment the index
+    idx++;
+    // write it to the CSV file
+    if (!myCSVStream.write(newCSVStr)) {
+      // if it fails, return, and the drain event below will restart the process
+      return;
     }
   }
+  // close the file
+  myCSVStream.end();
 }
 
-var loadLookObjs = (numBatches, perBatch) => {
-  // For each batch
-  for (let i = 0; i < numBatches; i++) {
-    // create perBatch look objects and load them into the looksArr
-    let startingIndex = i * perBatch;
-    createNLookObjs(perBatch, startingIndex);
-    // load the looksArr into the database
-    //console.log(JSON.stringify(looksArr));
-    Looks.bulkCreate(looksArr)
-    .then(() => {
-      //console.log('Successfully added Looks');
-    })
-    .catch(err => {
-      console.log('In catch');
-      console.log('ERROR: ', err);
-    })
-  }
-}
+// Keeping this in case I want to switch back to batches
+// However, the way way I have to use drain to restart the write process when it fails seems to prohibit using 
+// batches unless some of these parameters were moved to the global scope
+// var loadBatches = (numBatches, perBatch) => {
+//   // For each batch
+//   for (let i = 0; i < numBatches; i++) {
+//     // create perBatch look objects and load them into the looksArr
+//     let startingIndex = i * perBatch;
+//     createAndWriteNCSVStrings(perBatch, startingIndex);
+//   }
+// }
 
 
-// looksObj is now a global array declared above
-// Total number of objects to generate
-let totalToGenerate = 1000;
-// Total number of objects before recycling; assume divides equally into TotalToGenerate
-let totalPerBatch = 100;
+// // Total number of objects to generate
+// let totalToGenerate = 100;
+// // Total number of objects before recycling; assume divides equally into TotalToGenerate
+// let totalPerBatch = 100;
 
-console.log(`totalToGenerate = ${totalToGenerate}, totalPerBatch = ${totalPerBatch}, Batches = ${totalToGenerate/totalPerBatch}`);
+// console.log(`totalToGenerate = ${totalToGenerate}, totalPerBatch = ${totalPerBatch}, Batches = ${totalToGenerate/totalPerBatch}`);
+
 console.log('starting -----')
 console.time('createAndWriteData');
 
-// load 
-loadLookObjs(totalToGenerate/totalPerBatch, totalPerBatch);
+// When finished writing to the file, stop the timer and end the process
+myCSVStream.on('finish', () => {
+  console.timeEnd('createAndWriteData');
+  process.exit(0);
+});
 
+// If the generation of the data is faster than the writing to the file, the write will fail. 
+// This will start it back up again
+myCSVStream.on('drain', () => {
+  createAndWriteNCSVStrings();
+});
 
-//str = JSON.stringify(finalLooksArr);
+// Start creating and writing to the file
+createAndWriteNCSVStrings();
 
-//console.log(str);
+// load batches 
+// loadBatches(totalToGenerate/totalPerBatch, totalPerBatch);
 
-// write headers and footers for the file so I can import it? Assign it to a variable?
-
-// fs.writeFile('./database/generatedData.txt', str, function(err) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log('The file was saved!');
-// });
-
-console.timeEnd('createAndWriteData');
